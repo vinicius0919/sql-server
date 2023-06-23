eb1722b9d6130a1884e086a7cf46159396ffb321
vinicius0919 <viniciuslima0919@gmail.com>
2023-06-23 02:20:17 -0300

    OTIMIZAÇÃO DE SEGURANÇA BÁSICA

--- api/db.js
+++ api/db.js
@@ -2,10 +2,10 @@ const mysql =require( "mysql");
 
 const db = mysql.createConnection(
     {
-        host: "localhost",
-        user: "root",
-        password:"root",
-        database: "sistema"
+        host: process.env.HOST,
+        user: process.env.USER,
+        password:process.env.PASSWORD,
+        database: process.env.DATABASE
     }
 );
 
